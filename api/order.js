5f4c61f5c82769d97309b5090e6b7977390d0e90
netotchel-hub <netotchel@gmail.com>
2026-02-06 21:34:15 +0000

    Delete api/order.js

--- api/order.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    // Здесь будет обработка заказов
-    return res.status(200).json({ success: true });
-  }
-  return res.status(405).json({ error: 'Method not allowed' });
-}
